51043188c3e7586747c440f25175c64496d7658e
Denis Kravchenko <d.kravchenko@ivideon.com>
2018-08-13 19:06:59 +0300

    delete commented code

--- src/image.c
+++ src/image.c
@@ -1759,42 +1759,7 @@ image resize_image(image im, int w, int h)
         val = get_pixel(im, im.w-1, im.h-1, k);
         set_pixel(resized, w-1, h-1, k, val);
     }
-/*
-    for(k = 0; k < im.c; ++k){
-        for(r = 0; r < im.h; ++r){
-            for(c = 0; c < w; ++c){
-                float val = 0;
-                if(c == w-1 || im.w == 1){
-                    val = get_pixel(im, im.w-1, r, k);
-                } else {
-                    float sx = c*w_scale;
-                    int ix = (int) sx;
-                    float dx = sx - ix;
-                    val = (1 - dx) * get_pixel(im, ix, r, k) + dx * get_pixel(im, ix+1, r, k);
-                }
-                set_pixel(part, c, r, k, val);
-            }
-        }
-    }
-    for(k = 0; k < im.c; ++k){
-        for(r = 0; r < h; ++r){
-            float sy = r*h_scale;
-            int iy = (int) sy;
-            float dy = sy - iy;
-            for(c = 0; c < w; ++c){
-                float val = (1-dy) * get_pixel(part, c, iy, k);
-                set_pixel(resized, c, r, k, val);
-            }
-            if(r == h-1 || im.h == 1) continue;
-            for(c = 0; c < w; ++c){
-                float val = dy * get_pixel(part, c, iy+1, k);
-                add_pixel(resized, c, r, k, val);
-            }
-        }
-    }
 
-    free_image(part);
-    */
     return resized;
 }
 
